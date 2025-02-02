c238465c59193aa874d2148b41788994d32dd628
aerosystems <art_k0st@icloud.com>
2025-02-02 18:06:21 +0200

    add logger logrus middleware

--- http_server/server.go
+++ http_server/server.go
@@ -3,7 +3,9 @@ package httpserver
 import (
 	"context"
 	"fmt"
+	"github.com/go-logrusutil/logrusutil/logctx"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 )
@@ -20,11 +22,35 @@ func NewHTTPServer(cfg *Config, log *logrus.Logger, opts ...Option) *Server {
 		log:  log,
 		addr: fmt.Sprintf("%s:%s", cfg.Host, cfg.Port),
 	}
+
 	server.srv.HideBanner = true
 	server.srv.HidePort = true
+
 	for _, opt := range opts {
 		opt(server)
 	}
+
+	server.srv.Use(middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
+		LogURI:    true,
+		LogStatus: true,
+		LogValuesFunc: func(c echo.Context, values middleware.RequestLoggerValues) error {
+			log.WithFields(logrus.Fields{
+				"URI":    values.URI,
+				"status": values.Status,
+			}).Info("request")
+
+			return nil
+		},
+	}))
+
+	server.srv.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			c.Request().WithContext(logctx.New(c.Request().Context(), logrus.NewEntry(log)))
+			return next(c)
+		}
+	})
+
+	server.srv.Use(middleware.Recover())
 
 	return server
 }
